6f7a7d74c594ae39e36bcd447af4a537a06e5271
nishaprincess <rajashreem52@gmail.com>
2019-12-03 23:16:03 +0600

    first commit

---
+++ Vset.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dp;
+
+/**
+ *
+ * @author acer
+ */
+public class Vset {
+    
+    int u;
+    int []nu=new int[10000];
+    int [] n2u=new int[10000];
+    
+    int []b=new int[10000];
+    int []uset=new int[10000];
+    int []fset=new int[10000];
+    int nu_size;
+    int n2u_size;
+    int b_size;
+    int u_size;
+    
+    
+    
+    
+    
+    
+}
+
+
